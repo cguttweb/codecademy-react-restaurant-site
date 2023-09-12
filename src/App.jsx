a4aa4e1faa6da4c91aa7d4ef90b5d9029bbd4095
Chloe <cguttweb@pm.me>
2023-09-12 22:32:16 +0100

    added searchbar component and started on styling with css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Business from './components/Business'
+import SearchBar from './components/SearchBar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,14 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <SearchBar />
       <Business />
     </>
   )
